3d0699cf591d610f4f27c1ba78dcdd29ac86b08a
Marek Kochanowicz <sirherrbatka@gmail.com>
2014-04-06 12:00:53 +0200

    more keyboard navigation

--- app/fsview.cpp
+++ app/fsview.cpp
@@ -2,8 +2,10 @@
 #include <QVBoxLayout>
 #include <QApplication>
 #include <QRegExp>
+#include <iostream>
 
 #include "settingsmanager.h"
+#include "maincontroler.h"
 
 FSView::FSView() :
 mList(),
@@ -21,14 +23,20 @@ mModel()
     mModel.setFilter(QDir::NoDotAndDotDot |
                      QDir::AllDirs);
     mList.setModel(&mModel);
+    mList.setSelectionMode(QAbstractItemView::SingleSelection);
     mList.setUniformItemSizes(true);
     connect(&mEnterField, SIGNAL(textChanged(QString)), this, SLOT(createMatching(QString)));
     QVBoxLayout* layout = new QVBoxLayout(this);
+
     mList.setFocusPolicy(Qt::NoFocus);
     mEnterField.setFocusPolicy(Qt::NoFocus);
+
     mEnterField.hide();
     layout->addWidget(&mList, 0);
     layout->addWidget(&mEnterField, 1);
+
+    this->setFocusPolicy(Qt::WheelFocus);
+    this->setFocus(Qt::OtherFocusReason);
 }
 
 void FSView::broadMatching(const QString& string)
@@ -73,6 +81,42 @@ void FSView::keyPressEvent(QKeyEvent* event)
         close();
         return;
     }
+
+    const QModelIndex& current = mList.currentIndex();
+    if (!current.isValid())
+    {
+        mList.setCurrentIndex(mModel.index(0, 0, mModel.index(mModel.rootPath())));
+    }
+
+
+    if(event->key() == Qt::Key_Up)
+    {
+        if (mList.currentIndex().isValid())
+        {
+            if (mList.currentIndex().row() == 0)
+            {
+                mList.setCurrentIndex(mModel.index(mModel.rowCount(mModel.index(mModel.rootPath()))-1, 0, mModel.index(mModel.rootPath())));
+            } else {
+                mList.setCurrentIndex(mModel.index(current.row()-1, 0, mModel.index(mModel.rootPath())));
+            }
+        }
+        return;
+    }
+
+    if (event->key() == Qt::Key_Down)
+    {
+        if (mList.currentIndex().isValid())
+        {
+            if (mList.currentIndex().row() == mModel.rowCount(mModel.index(mModel.rootPath()))-1)
+            {
+                mList.setCurrentIndex(mModel.index(0, 0, mModel.index(mModel.rootPath())));
+            } else {
+                mList.setCurrentIndex(mModel.index(current.row()+1, 0, mModel.index(mModel.rootPath())));
+            }
+        }
+        return;
+    }
+
     if (mSearchMode)
     {
         if (event->key() == Qt::Key_Return)
@@ -80,13 +124,11 @@ void FSView::keyPressEvent(QKeyEvent* event)
             mSearchMode = false;
             mEnterField.hide();
         } else {
-            if (event->text().isEmpty())
+            if (!event->text().isEmpty())
             {
-                QDialog::keyPressEvent(event);
-                return;
+                mSearchText = mEnterField.text();
+                QApplication::sendEvent(&mEnterField, event);
             }
-            mSearchText = mEnterField.text();
-            QApplication::sendEvent(&mEnterField, event);
         }
         QDialog::keyPressEvent(event);
     } else {
@@ -95,15 +137,47 @@ void FSView::keyPressEvent(QKeyEvent* event)
             mSearchMode = true;
             mEnterField.show();
             QDialog::keyPressEvent(event);
-        } else {
-            if (!event->text().isEmpty() &&
-                event->key() != Qt::Key_Return &&
-                event->key() != Qt::Key_Backspace &&
-                event->key() != Qt::Key_Delete)
+            return;
+        }
+
+        if (!mList.currentIndex().isValid())
+        {
+            const QModelIndex& first = mModel.index(0, 0);
+            if (first.isValid())
             {
-                QApplication::sendEvent(&mList, event);
-                QDialog::keyPressEvent(event);
+                mList.setCurrentIndex(first);
             }
+        }
+
+        if (event->key() == Qt::Key_Return)
+        {
+            const QModelIndex& current = mList.currentIndex();
+            const QString& path = mModel.filePath(current);
+            mModel.setRootPath(path);
+            mList.setCurrentIndex(mModel.index(0, 0, mModel.index(mModel.rootPath())));
+            return;
+        }
+
+        if (event->key() == Qt::Key_Backspace)
+        {
+            return;
+        }
+
+        if (event->key() == Qt::Key_Space)
+        {
+            const QModelIndex& current = mList.currentIndex();
+            const QString& path = mModel.filePath(current);
+            QList<QUrl> list;
+            list.append(QUrl(path));
+            MainControler::getMainControler()->addPathToPlayList(list);
+            return;
+        }
+
+        if (!event->text().isEmpty() &&
+            event->key() != Qt::Key_Delete) //as above
+        {
+            QApplication::sendEvent(&mList, event);
+            QDialog::keyPressEvent(event);
         }
     }
 }
